a7b33fc1c7f05202166b6b7d47c6df834d83671b
RoyJumah <royjumah17@gmail.com>
2023-01-20 07:11:48 +0300

    Add event handlers to form

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -1,23 +1,64 @@
-import React, { Fragment } from "react";
+import React, { useState } from "react";
 import "./BookForm.css";
 
-const BookForm = () => (
-  <>
-    <form>
-      <h2>ADD NEW BOOK</h2>
-      <div className="new-expense__controls">
-        <div className="new-expense__control">
-          <input type="text" placeholder="Book Title" />
+const BookForm = (props) => {
+  const [enteredTitle, setEnteredTitle] = useState({
+    bookTitle: "",
+  });
+
+  const [enteredAuthor, setEnteredAuthor] = useState({
+    bookAuthor: "",
+  });
+
+  const titleChangeHandler = (event) => {
+    setEnteredTitle(event.target.value);
+  };
+
+  const authorChangeHandler = (event) => {
+    setEnteredAuthor(event.target.value);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const bookData = {
+      bookTitle: enteredTitle,
+      bookAuthor: enteredAuthor,
+    };
+
+    props.onSaveBookData(bookData);
+    setEnteredTitle("");
+    setEnteredAuthor("");
+  };
+
+  return (
+    <>
+      <form onSubmit={submitHandler}>
+        <h2>ADD NEW BOOK</h2>
+        <div className="new-expense__controls">
+          <div className="new-expense__control">
+            <input
+              type="text"
+              placeholder="Book Title"
+              value={enteredTitle}
+              onChange={titleChangeHandler}
+            />
+          </div>
+          <div className="new-expense__control">
+            <input
+              type="text"
+              placeholder="Author"
+              value={enteredAuthor}
+              onChange={authorChangeHandler}
+            />
+          </div>
+          <div className="new-book__actions">
+            <button type="submit">Add Book</button>
+          </div>
         </div>
-        <div className="new-expense__control">
-          <input type="text" placeholder="Author" />
-        </div>
-        <div className="new-book__actions">
-          <button type="submit">Add Book</button>
-        </div>
-      </div>
-    </form>
-  </>
-);
+      </form>
+    </>
+  );
+};
 
 export default BookForm;
