a4fde462bc520ab99c9252bd9811933aa7eb5be2
RoyJumah <royjumah17@gmail.com>
2023-01-23 18:30:13 +0300

    Add form and user input validation

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -5,7 +5,8 @@ import PropTypes from 'prop-types';
 const BookForm = ({ onAddBook }) => {
   const [enteredTitle, setEnteredTitle] = useState('');
   const [enteredAuthor, setEnteredAuthor] = useState('');
-
+  const [enteredTitleIsValid, setEnteredTitleIsValid] = useState(true);
+  const [enteredAuthorIsValid, setEnteredAuthorIsValid] = useState(true);
   const titleChangeHandler = (event) => {
     setEnteredTitle(event.target.value);
   };
@@ -16,31 +17,48 @@ const BookForm = ({ onAddBook }) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
+    if (enteredTitle.trim() === '' || enteredAuthor.trim() === '') {
+      setEnteredTitleIsValid(false);
+      setEnteredAuthorIsValid(false);
+      return;
+    }
+
+    setEnteredTitleIsValid(true);
+    setEnteredAuthorIsValid(true);
     onAddBook({ title: enteredTitle, author: enteredAuthor });
     setEnteredAuthor('');
     setEnteredTitle('');
   };
+  const nameInputClasses = enteredAuthorIsValid && enteredAuthorIsValid
+    ? 'new-expense__control'
+    : ' invalid';
 
   return (
     <>
       <form onSubmit={submitHandler}>
         <h2>ADD NEW BOOK</h2>
         <div className="new-expense__controls">
-          <div className="new-expense__control">
+          <div className={nameInputClasses}>
             <input
               type="text"
               placeholder="Book Title"
               value={enteredTitle}
               onChange={titleChangeHandler}
             />
+            {!enteredTitleIsValid && (
+              <p className="error-text">Title must not be empty</p>
+            )}
           </div>
-          <div className="new-expense__control">
+          <div className={nameInputClasses}>
             <input
               type="text"
               placeholder="Author"
               value={enteredAuthor}
               onChange={authorChangeHandler}
             />
+            {!enteredAuthorIsValid && (
+              <p className="error-text">Author must not be empty .</p>
+            )}
           </div>
           <div className="new-book__actions">
             <button type="submit">Add Book</button>
