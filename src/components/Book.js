b9d53dc1b18ae4488c522339ac5afeddd59c1103
RoyJumah <royjumah17@gmail.com>
2023-01-24 19:35:59 +0300

    Remove some bad code

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,49 +1,36 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import Books from './Books';
 import BookForm from './BookForm';
 
-class Book extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      books: [
-        {
-          id: uuidv4(),
-          title: 'When The Sun Goes Down',
-          author: 'Gwynne Forster',
-        },
-        {
-          id: uuidv4(),
-          title: 'To Kill A Mocking Bird',
-          author: 'Harper Lee',
-        },
-      ],
-    };
-  }
+const Book = () => {
+  const [books, setBooks] = useState([
+    {
+      id: uuidv4(),
+      title: 'When The Sun Goes Down',
+      author: 'Gwynne Forster',
+    },
+    {
+      id: uuidv4(),
+      title: 'To Kill A Mocking Bird',
+      author: 'Harper Lee',
+    },
+  ]);
 
-  addBook = (book) => {
-    this.setState((prevState) => ({
-      ...prevState,
-      books: [...prevState.books, { ...book, id: uuidv4() }],
-    }));
+  const addBook = (book) => {
+    setBooks([...books, { ...book, id: uuidv4() }]);
   };
 
-  deleteBook = (id) => {
-    this.setState((prevState) => ({
-      books: prevState.books.filter((book) => book.id !== id),
-    }));
+  const deleteBook = (id) => {
+    setBooks(books.filter((book) => book.id !== id));
   };
 
-  render() {
-    const { books } = this.state;
-    return (
-      <div>
-        <Books books={books} deleteBook={this.deleteBook} />
-        <BookForm onAddBook={this.addBook} />
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <Books books={books} deleteBook={deleteBook} />
+      <BookForm onAddBook={addBook} />
+    </div>
+  );
+};
 
 export default Book;
